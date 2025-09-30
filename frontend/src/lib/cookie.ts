325a039bb27b2e538ec830eca60766967d052de2
sasha <sasha@HOME-PC>
2025-09-30 22:36:18 +0500

    create

--- frontend/src/lib/cookie.ts
+++
@@ -1,8 +0,0 @@
-export function setTokenCookie(token: string) {
-    document.cookie = `token=${token}; path=/; Secure; SameSite=Lax`;
-}
-
-export function getTokenCookie(): string | null {
-    const match = document.cookie.match(new RegExp('(^| )token=([^;]+)'));
-    return match ? match[2] : null;
-}
